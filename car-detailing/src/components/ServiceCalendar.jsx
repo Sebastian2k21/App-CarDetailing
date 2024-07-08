53f4e1c89d298c550a237b928b26314e34c23e70
Sebastian Żądło <seba0071@interia.pl>
2024-07-08 20:33:55 +0200

    update calender

--- car-detailing/src/components/ServiceCalendar.jsx
+++ car-detailing/src/components/ServiceCalendar.jsx
@@ -1,23 +1,56 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useApiClient } from "../api/ApiClientContext";
 import {DayPilotCalendar} from "@daypilot/daypilot-lite-react";
 
 
+const getCurrentDate = () => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today =yyyy + '-' + mm + '-' + dd;
+    return today
+}
+
+const getOffsetDate = (offset) => {
+    var today = new Date();
+    var dd = String(today.getDate() + offset).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today =yyyy + '-' + mm + '-' + dd;
+    return today
+}
+
+
 const ServiceCalendar = ({ serviceId }) => {
+    const calendarRef = useRef()
     const apiClient = useApiClient()
     const [schedules, setSchedules] = useState({})
-    const [calendar, setCalendar] = useState();
+    const [calendarConfig, setCalendarConfig] = useState({
+        startDate: getCurrentDate(),  
+        weekStarts: 1, viewType: "Week", 
+        eventMoveHandling: "Disabled", 
+        eventResizeHandling: "Disabled", 
+        timeRangeSelectedHandling: "Disabled", eventClickHandling: "Disabled", eventDeleteHandling: "Disabled", 
+        eventHoverHandling: "Disabled", eventRightClickHandling: "Disabled", eventDoubleClickHandling: "Disabled"})
 
 
     const getAvailableSchedules = useCallback(async () => {
-        const schedules = await apiClient.availableSchedules(serviceId, '2024-07-01', '2024-07-07')
+        const startDate = getCurrentDate();
+        const endDate = getOffsetDate(7)
+        const schedules = await apiClient.availableSchedules(serviceId, startDate, endDate)
         setSchedules(schedules)
-        console.log(calendar)
+
+        calendarRef.current.control.update({startDate, events: schedules});
     }, [apiClient, serviceId])
 
     useEffect(() => {
         getAvailableSchedules()
     }, [getAvailableSchedules])
+
+    useEffect(() => {
+
+      }, []);
 
     return <div>
         {/* {Object.keys(schedules).map(day => (
@@ -31,7 +64,7 @@ const ServiceCalendar = ({ serviceId }) => {
             </div>
         ))} */}
 
-        <DayPilotCalendar  viewType="Week" controlRef={setCalendar}/>
+        <DayPilotCalendar ref={calendarRef} {...calendarConfig} />
     </div>
 }
 
